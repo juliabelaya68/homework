7b035d8e9fc6284abc6233277c7fbb7f40b146bc
uliabelaya68 <juliabelaya68@gmail.com>
2024-09-19 10:36:35 +0300

    2-nd course hw

---
+++ script.js
@@ -0,0 +1,60 @@
+//Задание 1//
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//Задание 2//
+const releaseIphone = 2007;
+console.log(releaseIphone);
+
+//Задание 3//
+const nameCreatorJs = 'Брендан Эйх';
+alert(nameCreatorJs);
+
+//Задание 4//
+let a = 10;
+let b = 2;
+
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+//Задание 5//
+let result = 2 ** 5;
+alert('результат задания равен ${result}');
+
+//Задание 6//
+let a = 9;
+let b = 2;
+let rest = a %
+    alert(rest);
+
+
+//Задание 7//
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num -= 1;
+num -= 1;
+alert(num);
+
+//Задание 8//
+let age = promt('Сколько вам лет?');
+alert(age);
+
+//Задание 9//
+const user = {
+    name: 'Olya',
+    age: 20,
+    isAdmin: true,
+}
+
+//Задание 10//
+let userName = prompt('Твое имя?');
+alert(`Привет, ${userName}!`);
+
+
