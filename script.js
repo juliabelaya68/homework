843b44217c1df274ba1c9e6fcc51b05cd49b4e9d
uliabelaya68 <juliabelaya68@gmail.com>
2024-10-16 17:00:50 +0300

    hw-6 доработка

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//игра угадай число
+//1игра угадай число
 function oneGame() {
     let n = Math.floor(Math.random() * 101);
     console.log(n);
@@ -20,67 +20,88 @@ function oneGame() {
     }
 }
 
-//игра камень ножницы бумага
+//5игра камень ножницы бумага
+function fifthGame() {
+    const arr = ['камень', 'ножницы', 'бумага'];
 
-const arr = ['камень', 'ножницы', 'бумага'];
+    function rockPaperScissors() {
 
-function ockPaperScissors() {
+        const useChoice = prompt('Сделай выбор! Камень,ножницы,бумага!');
 
-    const useChoice = prompt('Сделай выбор! Камень,ножницы,бумага!');
+        const random = Math.floor(Math.random() * arr.length);
 
-    const random = Math.floor(Math.random() * arr.length);
+        const pcCoice = arr[random];
 
-    const pcCoice = arr[random];
-
-    console.log(pcCoice);
-    console.log(useChoice);
+        console.log(pcCoice);
+        console.log(useChoice);
 
 
-    if (useChoice.toLowerCase() === pcCoice) {
-        alert(`ничья! компьютер выбрал тоже ${pcCoice}!`)
+        if (useChoice.toLowerCase() === pcCoice) {
+            alert(`ничья! компьютер выбрал тоже ${pcCoice}!`)
 
-    } else if ((useChoice.toLowerCase() === "камень" && pcCoice === "ножницы") ||
+        } else if ((useChoice.toLowerCase() === "камень" && pcCoice === "ножницы") ||
 
-        (useChoice.toLowerCase() === 'ножницы' && pcCoice === 'бумага') ||
+            (useChoice.toLowerCase() === 'ножницы' && pcCoice === 'бумага') ||
 
-        (useChoice.toLowerCase() === 'бумага' && pcCoice === 'камень')
-    ) {
+            (useChoice.toLowerCase() === 'бумага' && pcCoice === 'камень')
+        ) {
 
-        alert(`вы победили! выбор компьютера ${pcCoice}!`)
-    } else {
-        alert(`победил компьютер! выбор компьютера ${pcCoice}!`)
+            alert(`вы победили! выбор компьютера ${pcCoice}!`)
+        } else {
+            alert(`победил компьютер! выбор компьютера ${pcCoice}!`)
+        }
     }
-}
 
-rockPaperScissors();
-
-
-//Игра угадай число, нужно переделть и доработать не получается все связать вместе
-
-const arithmetic = ['+', '-', '*', '/'];
-
-const number1 = Math.floor(Math.random() * 11); Math.floor(Math.random() * 101);
-const number2 = Math.floor(Math.random() * 11);
-const generatesArithmetic = Math.floor(Math.random() * arithmetic.length);
-const generates = arithmetic[generatesArithmetic];
-
-function taskArithmetic() {
-    return `${number1} ${generates} ${number2}`;
-}
-alert(taskArithmetic());
-const result = taskArithmetic;
-
-
-const userAnswer = prompt("Посчитай и запиши ответ");
-if (isNaN(userAnswer)) {
-    alert('Ошибка ввода!Введи число');
-} else if (userAnswer === result) {
-    alert('Верный ответ!');
-} else {
-    alert('Не верно.Попробуй еще!');
+    rockPaperScissors();
 }
 
 
+//2Игра арифметика, заработала!
+function twoGame() {
+    const arithmetic = ['+', '-', '*', '/'];
+
+    const number1 = Math.floor(Math.random() * 11); Math.floor(Math.random() * 101);
+    const number2 = Math.floor(Math.random() * 11);
+    const generatesArithmetic = Math.floor(Math.random() * arithmetic.length);
+    const generates = arithmetic[generatesArithmetic];
+
+    function taskArithmetic() {
+        return (`${number1} ${generates} ${number2}`);
+    }
+    alert(taskArithmetic());
+    const result = eval(taskArithmetic());
 
 
-taskArithmetic();
+    const userAnswer = prompt("Посчитай и запиши ответ");
+    if (isNaN(userAnswer)) {
+        alert('Ошибка ввода!Введи число');
+    } else if (userAnswer == result) {
+        alert('Верный ответ!');
+    } else {
+        alert('Не верно.Попробуй еще!');
+    }
+
+    taskArithmetic();
+}
+
+
+
+//3 игра перевни текст
+
+function textGame() {
+
+    const userText = prompt('Введи текст и прочитай его наоборот');
+
+    function reverseText(str) {
+        let newText = "";
+
+        for (let i = str.length - 1; i >= 0; i--) {
+            newText += str[i];
+        }
+        return newText;
+    }
+
+    alert(reverseText(userText));
+}
+
+
